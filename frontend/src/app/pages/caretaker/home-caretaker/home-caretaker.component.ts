a6d3bce1d2b62023869397d6f72e0b92b9a4c365
George Yerontides <georgeyerontides@gmail.com>
2022-11-16 18:14:12 +0200

    Dynamic navbar changes
    use route to display different buttons at navbar
    
    Co-Authored-By: rbalaskas <69812822+rbalaskas@users.noreply.github.com>

--- frontend/src/app/pages/caretaker/home-caretaker/home-caretaker.component.ts
+++ frontend/src/app/pages/caretaker/home-caretaker/home-caretaker.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Event, NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 
 @Component({
@@ -7,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-caretaker.component.scss']
 })
 export class HomeCaretakerComponent implements OnInit {
-
-  constructor() { }
-
+  public subroute:string = "";
+  public route: string = "/observe/"
+  public alert:boolean = false;
+  constructor(private router: Router) { 
+    this.subroute = this.router.url; 
+    this.router.events.subscribe((event:Event) =>
+    {
+      if (event instanceof NavigationStart) {
+      
+        console.log('start');
+        this.subroute = this.router.url; 
+        console.log(this.router.url);
+        console.log(this.route.includes(this.subroute));
+      }
+      if (event instanceof NavigationEnd) {
+      
+        console.log('end');
+        this.subroute = this.router.url; 
+        console.log(this.router.url);
+        console.log(this.route);
+        console.log(this.route.includes(this.subroute));
+      }
+      
+    })
+   
+  }
   ngOnInit(): void {
+    
+  }
+
+  navigateHome(){
+    this.router.navigate(["/observe"],);
   }
 
 }
