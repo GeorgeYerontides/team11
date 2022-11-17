7456fb6b35c4653ad3dcff897b1d28d85e9df332
George Yerontides <georgeyerontides@gmail.com>
2022-11-17 17:58:41 +0200

    Dashboard elements
    added various dashboard elements

--- frontend/src/app/pages/caretaker/main-display/patient-dashboard/patient-dashboard.component.ts
+++ frontend/src/app/pages/caretaker/main-display/patient-dashboard/patient-dashboard.component.ts
@@ -8,7 +8,19 @@ import { PatientService } from 'src/app/global/services/patient/patients.service
   templateUrl: './patient-dashboard.component.html',
   styleUrls: ['./patient-dashboard.component.scss']
 })
-export class PatientDashboardComponent implements OnInit, OnChanges {
+export class PatientDashboardComponent implements OnInit {
+
+  heartRate:string = '98';
+  SpO2:string = '98';
+  stress:string ='Normal';
+  location:string = 'Living Room';
+  Age: string = '75';
+  Weight: string = '68';
+  Height: string = '1.63';
+  emergencyName:string ='Giorgos Lamprou';
+  emergencyPhone:string ='6982841943';
+  emergencyMail:string ='glamprou@gmail.com';
+  
   public currUser:User ;
   constructor(private patientService: PatientService, private route:ActivatedRoute) { 
     let name = this.route.snapshot.params['name']
@@ -19,7 +31,6 @@ export class PatientDashboardComponent implements OnInit, OnChanges {
     let name = this.route.snapshot.params['name']
     this.currUser = this.patientService.getUser(name);
   }
-  ngOnChanges(changes: SimpleChanges): void {
-  }
+
 
 }
